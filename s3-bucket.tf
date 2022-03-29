95d64cd5792ee868618405c0fd3819ea87ba3f46
Jon Latorre <jlatorre@irontec.com>
2022-03-29 15:04:28 +0000

    Ajustando el boke

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/irontec-demo-jlatorre/s3-bucket/aws"
   version = "2.8.0"
 
+  bucket_prefix = "${var.prefix}"
   bucket = "my-s3-bucket"
   acl    = "private"
 
